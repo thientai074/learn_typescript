9b3a239cd4952f2e9a3eac1642c8c4f03b557818
Ha VIet Hung <85442688+thientai074@users.noreply.github.com>
2022-04-01 14:40:15 +0700

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,76 +1,86 @@
-import { useState, FormEventHandler, FormEvent} from 'react'
-import axios from 'axios'
-import { useRouter} from 'next/router'
+import { useState, FormEvent} from 'react'
+import { useEffect} from 'react'
+import SingleCommentDetailPage from '../SingleCommentDetailPage'
+import { useDispatch, useSelector } from 'react-redux'
+import { addComment, getCommentsInThisImages } from '../../reduxstore/reducers/commentDetailPage'
+import { RootState } from '../../reduxstore'
+import Input from '../Input';
+import { CommentFormType } from '../../types/comment';
 
+const CommentForm = ({urlImageLink, image, objectId}: CommentFormType) => {  
+    const userComments = useSelector((state: RootState) => state.comments.allComments)
+    
+    const [comment, setComment] = useState<string>('')       
 
+    const commentForm = {
+        content: comment, 
+        objectId: image._id
+    }     
 
-const CommentForm = () => {
+    const dispatch = useDispatch()  
+  
+    useEffect(() => {
+        if(objectId) {
+        dispatch(getCommentsInThisImages(objectId))      
+        }        
+    }, [dispatch, objectId])
 
-    const [comment, setComment] = useState('')
+    const handleSubmitComment = (event : FormEvent<HTMLFormElement>) => {        
+        event.preventDefault()                 
+        dispatch(addComment(commentForm))
+        setComment('')      
+    } 
 
-    const handleSubmitComment = async (event : FormEvent<HTMLFormElement>) => {
-        
-        event.preventDefault()
-        
-        console.log("E: ", comment);
-        try {
-            await axios.post('http://localhost:4000/comment',{content: comment}, {withCredentials: true, headers:{"Access-Control-Allow-Credentials":true}} )
-         
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    const commentLength = (userComments.length > 1) 
 
+        ? ( `${userComments.length} comments`) 
+        : (userComments.length === 1) 
+        ? ('1 comment') 
+        : ''
 
-  return (
-        <div className='bg-white border-2 border-gray-200 mt-10 rounded-md justify-center px-20'>
-            <div className='flex justify-between pt-10'>
-                <p className='font-sans'>2 Comments</p>
-                <div className='flex'>
-                    <a className='mr-4' href="">Oldest</a>
-                    <a href="">Newest</a>
+    const commentTitle = (userComments.length === 0) 
+        ? ('Be the first to join the discussion')
+        : ''
+    
+    return (
+        <div className='bg-white border-2 border-gray-200 mt-10 rounded-md justify-center px-32 lg:px-56 h-[420px] overflow-y-scroll my-auto '>
+            <div className='flex  pt-10'>
+                <div className='mx-auto'>
+                    <p className='font-semibold'>
+                        {commentTitle}
+                    </p>                    
                 </div>
+                <p className='font-sans'>
+                {commentLength}
+                </p>
+                
             </div>
-            <div className=''>
-                <div className='flex mt-10 border-b-2 border-gray-200 pb-6'>
-                    <img className='rounded-full h-12 w-12' src="https://i.pinimg.com/originals/79/20/8a/79208ac14cd0682d093ca381d2a9a95f.jpg" alt="" />
-                    <div className='ml-4'>
-                        <h2 className='font-bold'>Meo Meo</h2>
-                        <p>Well done</p>
-                        <p>5 months ago</p>
-                    </div>
-
+            {urlImageLink && (
+                <div >
+                    {userComments && userComments.map(userComment => {   
+                        return(                    
+                            <SingleCommentDetailPage urlImageLink={urlImageLink} 
+                            userComment={userComment}                            
+                            key={userComment._id}  />   
+                        )}
+                     )}           
                 </div>
-                <div className='flex mt-10 border-b-2 border-gray-200 pb-6'>
-                    <img className='rounded-full h-12 w-12' src="https://i.pinimg.com/736x/8a/ed/fe/8aedfe469628f7427d2214ed966deaa2.jpg" alt="" />
-                    <div className='ml-4'>
-                        <h2 className='font-bold'>Meo Meo</h2>
-                        <p>Well done</p>
-                        <p>5 months ago</p>
-                    </div>
-
-                </div>
-                <div className='flex mt-10 '>
-                    <img className='rounded-full h-12 w-12' src="http://2.bp.blogspot.com/-hZqFkNjxxkQ/UbqN4w4HF2I/AAAAAAAABdI/SU0RTS5QJBk/s1600/cho.jpg" alt="" />
-                    <div className='ml-4'>
-                        <h2 className='font-bold'>Meo Meo</h2>
-                        <p>Well done</p>
-                        <p>5 months ago</p>
-                    </div>
-
-                </div>
-            </div>
-            <div className='mt-10 pb-10 relative '> 
-                <form className='flex-col flex' onSubmit={(event) => handleSubmitComment(event)} >
-                    <input 
-                        type="text" 
-                        className='h-20 w-full border-2 border-gray-200' 
-                        placeholder='Leave a comment, be nice'
-                        onChange={e => setComment(e.target.value)} />
+            )}
+            <div className='mt-10 relative '> 
+                <form className='flex-col flex' 
+                onSubmit={handleSubmitComment} 
+                >
+                    <Input 
+                        type="textarea"                       
+                        placeHolder='Leave a comment, be nice'
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}
+                        rows={4}                      
+                    />
                     <button 
                         className=" font-sans mt-4 top-20 w-20 h-12 self-end font-medium 
-                        text-white bg-indigo-600 py-0 px-2 rounded "
-                        // onClick={(event) => handleSubmitComment(event)}
+                        text-white bg-indigo-600  px-2 rounded "                           
+                        type='submit'                                        
                         >
                             Post It
                     </button>
@@ -81,3 +91,9 @@ const CommentForm = () => {
 }
 
 export default CommentForm
+    
+
+
+
+ 
+
